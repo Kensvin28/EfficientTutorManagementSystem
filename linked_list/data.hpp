c5d1e1fb293067c9da54617ca85f8ffd959d1c9b
kensvin <kensvin28@gmail.com>
2022-05-30 23:58:58 +0800

    Update data.hpp

--- linked_list/data.hpp
+++ linked_list/data.hpp
@@ -1,38 +1,6 @@
 #ifndef DATA_H
 #define DATA_H
 
-// #include <string>
-// using namespace std;
-
-// struct Tutor
-// {
-// 	int tutor_ID;
-// 	string name;
-// 	int date_joined;
-// 	int date_terminated;
-// 	double hourly_rate;
-// 	string phone;
-// 	string address;
-// 	int centre_code;
-// 	string centre_name;
-// 	int subject_code;
-// 	string subject_name;
-// 	int rating;
-// 	Tutor* next;
-// 	Tutor* prev;
-// };
-
-// struct Staff
-// {
-// 	int staff_id;
-// 	string staff_name;
-// 	string staff_position;
-// 	int centre_code;
-// 	string staff_password;
-// 	Staff* next;
-// 	Staff* prev;
-// };
-
 #include <string>
 
 struct Tutor
