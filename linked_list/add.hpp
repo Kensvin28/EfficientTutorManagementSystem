dd16b552175d36c972850aa3371b885321bd5f6d
KeVV19 <kevinmiharjo2002@gmail.com>
2022-06-02 22:59:49 +0800

    supporting header

---
+++ linked_list/add.hpp
@@ -0,0 +1,9 @@
+#ifndef ADD_H
+#define ADD_H
+
+#include "data.hpp"
+#include <string>
+
+Tutor* create_new_tutor_node(int, std::string, std::string, std::string, double, std::string, std::string, int, std::string, int, std::string, int);
+
+#endif
