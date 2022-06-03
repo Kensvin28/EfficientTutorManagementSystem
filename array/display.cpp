ca27e9f2d1f8959f9216d53431add194b8b8a5e9
kensvin <kensvin28@gmail.com>
2022-06-03 08:07:44 +0800

    array display

--- array/display.cpp
+++ array/display.cpp
@@ -6,7 +6,8 @@ using namespace std;
 //extern struct Tutor tutor_array[100];
 
 void display_detailed(Tutor* tutor_array, int index){
-	return;
+	cout << "| " << tutor_array[index].tutor_ID << "| " << tutor_array[index].name << "| " << tutor_array[index].date_joined << "| " + tutor_array[index].date_terminated << "| " << tutor_array[index].hourly_rate << "| " << tutor_array[index].phone <<
+		"| " << tutor_array[index].address << "| " << tutor_array[index].centre_code << "| " << tutor_array[index].centre_name << "| " << tutor_array[index].subject_code << "| " << tutor_array[index].subject_name << "| " << tutor_array[index].rating << "| " << endl;
 }
 void display_separator() {
 	cout << string(25, '=');
